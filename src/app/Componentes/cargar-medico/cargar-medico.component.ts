69fad6b591c4fb2e9df3b7de0c4c332d1603285c
fabele15 <fabele@mstech.la>
2025-12-10 23:58:04 -0300

    issues

--- src/app/Componentes/cargar-medico/cargar-medico.component.ts
+++ src/app/Componentes/cargar-medico/cargar-medico.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { MedicoService } from '../../Services/medico.service';
 import { EspecialidadService } from 'src/app/Services/especialidad.service';
 import { ObraSocialService } from 'src/app/Services/obra-social-service.service';
@@ -30,10 +31,9 @@ export class CargarMedicoComponent implements OnInit {
     private especialidadService: EspecialidadService,
     private obraSocialService: ObraSocialService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private location: Location
   ) {}
-
-  
 
   ngOnInit(): void {
     this.idMedico = this.route.snapshot.params['id'];
@@ -110,5 +110,9 @@ export class CargarMedicoComponent implements OnInit {
         }
       });
     }
+  }
+
+  goBack() {
+    this.location.back();  
   }
 }
